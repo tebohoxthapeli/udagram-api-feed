0a2f4500ac0924a503de2ee37c50f305be31e7d9
Teboho Thapeli <tm.thapeli@gmail.com>
2022-07-11 19:34:42 +0200

    -

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import cors, { CorsOptions } from "cors";
+import cors from "cors";
 
 import { sequelize } from "./sequelize";
 import { IndexRouter } from "./controllers/v0/index.router";
@@ -7,7 +7,7 @@ import { V0_FEED_MODELS } from "./controllers/v0/model.index";
 import { config } from "./config/config";
 
 (async () => {
-	sequelize.addModels(V0_FEED_MODELS);
+	await sequelize.addModels(V0_FEED_MODELS);
 	console.debug("Initialize database connection...");
 	await sequelize.sync();
 
@@ -17,23 +17,22 @@ import { config } from "./config/config";
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
 
-	const corsOptions: CorsOptions = {
-		origin: "*",
-		credentials: true,
-		optionsSuccessStatus: 200,
-		allowedHeaders: [
-			"Origin",
-			"X-Requested-With",
-			"Content-Type",
-			"Accept",
-			"Authorization",
-			"X-Access-Token",
-		],
-		preflightContinue: true,
-		methods: ["GET", "HEAD", "OPTIONS", "PUT", "PATCH", "POST", "DELETE"],
-	};
+	app.use(
+		cors({
+			allowedHeaders: [
+				"Origin",
+				"X-Requested-With",
+				"Content-Type",
+				"Accept",
+				"X-Access-Token",
+				"Authorization",
+			],
+			methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+			preflightContinue: true,
+			origin: "*",
+		})
+	);
 
-	app.use(cors(corsOptions));
 	app.use("/api/v0/", IndexRouter);
 
 	app.get("/", async (_, res) => {
